765b3b0c0a61c18985271b37e01e3ca8e4722798
Randy White <randy.white.1986@gmail.com>
2016-05-25 03:02:43 -0700

    changed the token structure. added old school bsd tree and queue macros in the project itself

--- src/include/tokenPackageStructure.h
+++ src/include/tokenPackageStructure.h
@@ -1,16 +1,31 @@
 /*
- *
+ * Created by Randy White
  */
 
 #ifndef _TOKEN_PACKAGE_
 #define _TOKEN_PACKAGE_
 
-#define TMP_MAX 2048 /* This is temporary, it probably needs to be expanded */
+#include "queue.h" //I'm lazy, just plopping this in the directory
+#include "tree.h" //I'm lazy, just plopping this in the directory
+
+#define HELL_MAX 2048 /* This is temporary, it probably needs to be expanded */
+
+	union pload {
+	/* These are the variables associated with the payloads
+	At the moment there are only strings, float matrices, a long double and long long int	
+	 */
+	char stringP[HELL_MAX];
+	float floatP[HELL_MAX][HELL_MAX];
+	long long int intP;	
+	long double doubleP;	
+	
+	};
+
 
 struct tokenPackage 
 {
 
-	 
+	//these are actually the tokens now
 	int index, typeId,fileDescriptor;
 	/* Index of the package, the type of package processing needed and 
 	file descriptor for various types of processing */
@@ -18,21 +33,28 @@ struct tokenPackage
 	pid_t processId; 
 	/* The process id for package, so we can track */ 
 	FILE *filePointer; /* The main file pointer which is C standard */
-	char filePathString[TMP_MAX];/* The main file path string */
+	char filePathString[HELL_MAX];/* The main file path string */
+	union pload payload;
 
-	union payload {
-	/* These are the variables associated with the payloads
-	At the moment there are only strings, float matrices, a long double and long long int	
-	 */
-	char stringP[TMP_MAX];
-	float floatP[TMP_MAX][TMP_MAX];
-	long long int intP;	
-	long double doubleP;	
-	
-	};
 
-struct tokenPackage *left, *right, *next; //these are used for linked lists
+	//~ struct tokenPackage *left, *right, *next; //these are used for linked lists //deprecated 
+
+	SIMPLEQ_ENTRY(tokenPackage) links; //I guess set up the queue here //this is just using the system macros right here 
+
 };
 
-/* This was awesome */
+
+
+	struct tokenPackageQueue_t {
+
+		SIMPLEQ_HEAD(tokenPackageQueue, tokenPackage) package;
+		//this is the container right here for the queue 
+	};
+
+
+/* This was awesome
+ * Using internal queue stuff now */
 #endif _TOKEN_PACKAGE_
+
+
+
