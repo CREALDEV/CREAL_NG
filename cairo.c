c897113c11891fd58935c623b5782e04e9c96f5a
Randall White <randallwhite@lbl.gov>
2016-08-18 17:00:01 -0700

    new libraries, and cairo stuff

--- cairo.c
+++ cairo.c
@@ -11,6 +11,8 @@
 #include <string.h>
 #include <SDL/SDL.h>
 #include <stdbool.h> 
+#include <time.h>
+
 
 #include <cairo/cairo.h>
 #include "src/include/cairosdl.h"
@@ -32,15 +34,18 @@ int screenGrab[SCREEN_WIDTH][SCREEN_HEIGHT]; //just take the x values of the scr
 //add a nice pi constant 
 #define M_PI 3.14159265358979323846
 
+//declarations for the voided function right here// we need this or else it will complain
 static void draw (cairo_t *cr);
 static void draw_screen (SDL_Surface *screen);
+static void drawBg (SDL_Surface *screen); 
+static void drawLs (SDL_Surface *screen);
 
 int pid;
 pid_t mainPid;
 bool mainGameLoopConditional;
 //begin
 
-SDL_Surface *hello, *screen, *sdlsurf;
+SDL_Surface *hello, *screen, *sdlsurf, *backGroundSurface, *nodeSurface;
 cairo_surface_t *cairosurf, *surface;
 cairo_t *cr, *tr;
 		//~ SDL_Surface*  = NULL;
@@ -49,20 +54,109 @@ int deltaX, deltaY;
  
 //gcc example.c -o example `pkg-config --cflags --libs gtk+-3.0`
 
+struct xContainerNode
+{
+		int x; //x offset
+		SPLAY_ENTRY(xContainerNode) entry; //the entries into the binary tree
+}; 
+	
+
+int returnRandIntInRange(int low, int high)
+{
+		volatile int randomVariable;
+		do
+		{
+			randomVariable = (rand() %  (high - low + 1 ) + low); //return this right here 
+		}
+		
+		while ( !(randomVariable < low && randomVariable > high ));
+		
+		return randomVariable; 
+		
+}
+
+
+
 struct graphicNodeData 
 { 
+		int x,y; //coordinates
 		int innerX, innerY, outerX, outerY; //this is for screen space 
-		int i, j, k,x,y; 
 		int nodeId; 
 		int numOfProperties; 
-		SDL_Surface *nodeSurface; //the surface for the main node 
 		char nodeName[256]; //I am only limiting this to 256 bytes
 		char fileName[2048]; 
-		SDL_Rect box;	
-		SPLAY_ENTRY(graphicNodeData) entry; //the entries into the binary tree
+		int connections[16]; 
+
 }; 
 
 
+struct xAndY
+{
+	int x, y; //just simple
+	
+}; 
+
+
+struct gNodeY { 
+	RB_ENTRY(gNodeY) entry; 
+	struct graphicNodeData *gnodePointer; //pointer of the gnode data
+	int i,y; 
+}; 
+ 
+
+
+struct gNodeX { 
+	RB_ENTRY(gNodeX) entry; 
+	RB_HEAD(gNodeYHead, gNodeY) head; 
+	int i,x; 
+}; 
+ 
+
+ 
+ 
+int 
+intcmpX(struct gNodeX *e1, struct gNodeX *e2) 
+{ 
+	if(e1->x < e2->x)
+	{
+		return -1;
+	}
+	else if(e1->x > e2->x)
+	{
+		
+		return 1;
+	}
+	else
+	{
+		
+		return 0; 
+	}
+} 
+ 
+ 
+int 
+intcmpY(struct gNodeY *e1, struct gNodeY *e2) 
+{ 
+		if(e1->y < e2->y)
+	{
+		return -1;
+	}
+	else if(e1->y > e2->y)
+	{
+		
+		return 1;
+	}
+	else
+	{
+		
+		return 0; 
+	}
+	
+} 
+ 
+
+
+struct graphicNodeData *mainGrid[SCREEN_WIDTH][SCREEN_HEIGHT]; 
 
 int main(int argc, char* argv[])
 {
@@ -76,33 +170,36 @@ int main(int argc, char* argv[])
 	
 	
 	//init
-	
+	 srand(time(0)); //set up a random number 
 	SDL_Init (SDL_INIT_VIDEO | SDL_INIT_TIMER);
 	screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP,  SDL_HWSURFACE | SDL_RESIZABLE );
     SDL_WM_SetCaption ("Paola", "ICON"); // setting window tittle 
 	
 	//creating surfaces
 	hello = SDL_CreateRGBSurface (0, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, 0x00FF0000, 0x0000FF00,0x000000FF, 0); /* Amask */
+	
+	nodeSurface = SDL_CreateRGBSurface (0, NODE_SURFACE_WIDTH, NODE_SURFACE_HEIGHT, SCREEN_BPP, 0x00FF0000, 0x0000FF00,0x000000FF, 0); /* Amask */
+	//~ SDL_SetColorKey( nodeSurface, SDL_SRCCOLORKEY, SDL_MapRGB(nodeSurface->format, 255, 0, 255) );
+	
+	backGroundSurface = SDL_CreateRGBSurface (0, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, 0x00FF0000, 0x0000FF00,0x000000FF, 0); /* Amask */
 	//~ surface = cairo_image_surface_create_for_data (sdlsurf->pixels, CAIRO_FORMAT_RGB24, sdlsurf->w, sdlsurf->h, sdlsurf->pitch);
 	//~ cr = cairo_create (surface);
-
+	
 	//^ for use with cairo 
 
 
-
-     //~ while(1)
-     //~ {
-		//~ 
-//~ 
-//~ 
-	//~ }
-
+	//this right here is for the single quadrants
+	box.w = NODE_SURFACE_WIDTH; 
+	box.h = NODE_SURFACE_HEIGHT; 
+	box.x = 0; 
+	box.y = 0; 
+	 
+	drawBg(backGroundSurface); 	
 
    while (SDL_WaitEvent (&event)) 
 	{
 
-		usleep(1); //using usleep
-	
+		
 		switch (event.type) {
 					
 						case SDL_QUIT:
@@ -121,8 +218,8 @@ int main(int argc, char* argv[])
 						
 							case SDL_MOUSEBUTTONDOWN:
 							{
-								
-								system("kst2"); 
+								puts("Mouse pressed down"); //the mouse was pressed down
+								system("kst2 /home/romxero/Documents/code/c/cBuildNu/681_0.dat -y 2"); 
 								break;	
 							}
 						
@@ -132,13 +229,25 @@ int main(int argc, char* argv[])
 							}
 
 				}
-	
-		
-		//~ draw_screen(hello); 
-		//drawNode();//this is a test function right here
-		//~ SDL_BlitSurface( hello, NULL, screen, NULL );
-		//~ SDL_Flip (screen); //make sure you flip the buffers to right here 
+				printf("Mouse X location : %d -- Mouse Y location %d \n", deltaX, deltaY); //show the locations
+				
+				//clear the screen first
+				SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 0, 0)); //clear
+				
+				//~ drawLs(backGroundSurface); 
+				draw_screen(screen); 
+				//drawNode();//this is a test function right here
+				//~ SDL_SetColorKey( nodeSurface, SDL_SRCCOLORKEY, SDL_MapRGB(nodeSurface->format, 255, 0, 255) );
+				
+				//~ SDL_BlitSurface( backGroundSurface, NULL, screen, NULL );
+				//~ SDL_BlitSurface( hello, NULL, screen, NULL );
+				//~ SDL_BlitSurface( nodeSurface, NULL, screen, &box );
+				SDL_Flip (screen); //make sure you flip the buffers to right here 
 
+	
+		usleep(1); //using usleep
+
+	//~ SDL_Delay( 2000 ); //this was way uneeded 
 	
 	}
 
@@ -154,7 +263,7 @@ int main(int argc, char* argv[])
 
 
 
-static void drawNode(cairo_t *cr)
+static void __drawNode(cairo_t *cr)
 {
 	//this function is used to draw the node only
 	
@@ -164,13 +273,35 @@ static void drawNode(cairo_t *cr)
 
 	cairo_set_source_rgba (cr, 0.001, 0.005, 1.0,.7);
 	cairo_set_line_width (cr, 10.0);
-	cairo_arc (cr, 128.0, 128.0, 76.8, 0, 2 * M_PI);
+	cairo_arc (cr, 128.0, 128.0, 12, 0, 2 * M_PI);
 
 	cairo_fill (cr);
 	
 	cairo_set_source_rgba (cr, 1.0, 0.5, 0,.5);
-	//~ cairo_set_line_width (cr, 10.0);
-	cairo_arc (cr, 128.0, 128.0, 76.8, 0, 2 * M_PI);
+	cairo_set_line_width (cr, 10.0);
+	cairo_arc (cr, 128.0, 128.0, 12, 0, 2 * M_PI);
+
+	cairo_stroke (cr);
+
+}//this is a test function right here
+
+static void drawNode(cairo_t *cr, double xOffset, double yOffset)
+{
+	//this function is used to draw the node only
+	
+	//this stuff below is for the background, and is deprecated
+	//~ cairo_set_source_rgba (cr, 1, 1, 1, .1);
+	//~ cairo_paint (cr);
+
+	cairo_set_source_rgba (cr, 0.001, 0.005, 1.0,.7);
+	cairo_set_line_width (cr, 10.0);
+	cairo_arc (cr, xOffset, yOffset, 12, 0, 2 * M_PI);
+
+	cairo_fill (cr);
+	
+	cairo_set_source_rgba (cr, 1.0, 0.5, 0,.5);
+	cairo_set_line_width (cr, 10.0);
+	cairo_arc (cr, xOffset, yOffset, 12, 0, 2 * M_PI);
 
 	cairo_stroke (cr);
 
@@ -195,13 +326,13 @@ drawConnections (cairo_t *cr)
 
 
 static void
-drawText (cairo_t *cr, const char *string)
+drawText (cairo_t *cr, const char *string, double xOffset, double yOffset)
 {
 	
 	cairo_select_font_face (cr, "serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
-    cairo_set_font_size (cr, 32.0);
+    cairo_set_font_size (cr, 12.0);
     cairo_set_source_rgba (cr, 0.001, 0.005, 1.0,.5);
-    cairo_move_to (cr, 200.0, 50.0);
+    cairo_move_to (cr, xOffset, yOffset);
     cairo_show_text (cr, string);
 	
 }
@@ -209,12 +340,35 @@ drawText (cairo_t *cr, const char *string)
 
 
 static void
-drawBg (cairo_t *cr)
+__drawBg (cairo_t *cr)
 {
 	
 	//paint the background
 cairo_set_source_rgb (cr, 1, 1, 1);
 cairo_paint (cr);
+}
+
+static void
+__drawBg2 (cairo_t *cr)
+{
+	
+cairo_set_source_rgb (cr, 1, 0, 1);
+	//paint the background
+cairo_paint (cr);
+}
+
+
+static void
+drawLines (cairo_t *cr)
+{
+	cairo_set_line_width (cr, 1.0);
+	cairo_set_source_rgba (cr, 0, 0, 0,.5);
+	//paint the background
+	
+	cairo_move_to(cr,513,302); 
+	cairo_line_to(cr,750,321); 
+	
+	cairo_stroke (cr);
 }
 
 
@@ -239,28 +393,7 @@ cairo_stroke (cr);
     cairo_move_to (cr, 200.0, 50.0);
     cairo_show_text (cr, "Pancrease_Test");
 
-//~ cairo_select_font_face (cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
-                               //~ CAIRO_FONT_WEIGHT_BOLD);
-//~ cairo_set_font_size (cr, 90.0);
-//~ 
-//~ cairo_move_to (cr, 10.0, 135.0);
-//~ cairo_show_text (cr, "Hello");
-//~ 
-//~ cairo_move_to (cr, 70.0, 165.0);
-//~ cairo_text_path (cr, "void");
-//~ cairo_set_source_rgb (cr, 0.5, 0.5, 1);
-//~ cairo_fill_preserve (cr);
-//~ cairo_set_source_rgb (cr, 0, 0, 0);
-//~ cairo_set_line_width (cr, 2.56);
-//~ cairo_stroke (cr);
-//~ 
-//~ /* draw helping lines */
-//~ cairo_set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
-//~ cairo_arc (cr, 10.0, 135.0, 5.12, 0, 2*M_PI);
-//~ cairo_close_path (cr);
-//~ cairo_arc (cr, 70.0, 165.0, 5.12, 0, 2*M_PI);
-//~ cairo_fill (cr);
-	
+
 }
 
 static void
@@ -275,13 +408,23 @@ draw_screen (SDL_Surface *screen)
 
         //~ cairo_scale (cr, screen->w, screen->h);
       
-		drawBg(cr);
-        drawNode(cr);
-		drawText(cr,"Pancrease"); 
+		__drawBg(cr);
+		//~ cairo_set_source_rgba (cr, 1, 1, 1, 1);
+		
+		drawLines(cr);		
+        drawNode(cr,513,302);
+		drawText(cr,"Node 1",533,282); 
+
+
+        drawNode(cr,750,321);
+		drawText(cr,"Node 2",770,301); 
+
+		
         status = cairo_status (cr);
         cairosdl_destroy (cr);
     }
     SDL_UnlockSurface (screen);
+    //~ SDL_SetColorKey( screen, SDL_SRCCOLORKEY, SDL_MapRGB(screen->format, 255, 0, 255) );
     //~ SDL_Flip (screen);
 
     /* Nasty nasty error handling. */
@@ -339,3 +482,62 @@ intcmp(struct graphicNodeData *e1, struct graphicNodeData *e2)
 	
 }; 
 
+
+
+static void
+drawBg (SDL_Surface *screen)
+{
+    cairo_t *cr;
+    cairo_status_t status;
+
+    /* Create a cairo drawing context, normalize it and draw a clock. */
+    SDL_LockSurface (screen); {
+        cr = cairosdl_create (screen);
+
+        cairo_scale (cr, screen->w, screen->h);
+      
+		__drawBg(cr);
+        //~ drawNode(cr);
+		//~ drawText(cr,"Pancrease"); 
+        status = cairo_status (cr);
+        cairosdl_destroy (cr);
+    }
+    SDL_UnlockSurface (screen);
+  
+
+    /* Nasty nasty error handling. */
+    if (status != CAIRO_STATUS_SUCCESS) {
+        fprintf (stderr, "Unable to create or draw with a cairo context "
+                 "for the screen: %s\n",
+                 cairo_status_to_string (status));
+        exit (1);
+    }
+}
+
+
+
+
+
+
+//~ cairo_select_font_face (cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
+                               //~ CAIRO_FONT_WEIGHT_BOLD);
+//~ cairo_set_font_size (cr, 90.0);
+//~ 
+//~ cairo_move_to (cr, 10.0, 135.0);
+//~ cairo_show_text (cr, "Hello");
+//~ 
+//~ cairo_move_to (cr, 70.0, 165.0);
+//~ cairo_text_path (cr, "void");
+//~ cairo_set_source_rgb (cr, 0.5, 0.5, 1);
+//~ cairo_fill_preserve (cr);
+//~ cairo_set_source_rgb (cr, 0, 0, 0);
+//~ cairo_set_line_width (cr, 2.56);
+//~ cairo_stroke (cr);
+//~ 
+//~ /* draw helping lines */
+//~ cairo_set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
+//~ cairo_arc (cr, 10.0, 135.0, 5.12, 0, 2*M_PI);
+//~ cairo_close_path (cr);
+//~ cairo_arc (cr, 70.0, 165.0, 5.12, 0, 2*M_PI);
+//~ cairo_fill (cr);
+	
--- treeStuff.c
+++ treeStuff.c
@@ -2,37 +2,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include "tree.h"
-
-
+#include "src/include/tree.h"
+#include <err.h> 
 
 struct graphicNodeData 
 { 
+		int x,y; //coordinates
 		int innerX, innerY, outerX, outerY; //this is for screen space 
-		int i, j, k,x,y; 
 		int nodeId; 
 		int numOfProperties; 
-		SDL_Surface *nodeSurface; //the surface for the main node
-		SDL_Rect box; 
 		char nodeName[256]; //I am only limiting this to 256 bytes
 		char fileName[2048]; 
-		SPLAY_ENTRY(graphicNodeData) entry; //the entries into the binary tree
+		int connections[16]; 
+
+}; 
+
+
+struct xAndY
+{
+	int x, y; //just simple
 	
 }; 
 
 
-struct testStructure 
-{
-	SPLAY_ENTRY(testStructure) entry; //the entries into the binary tree
-	int i, j, k,x,y; 
-	char shitToType[256]; 
-	void *something; 
+struct gNodeY { 
+	RB_ENTRY(gNodeY) entry; 
+	struct graphicNodeData *gnodePointer; //pointer of the gnode data
+	int i,y; 
 }; 
+ 
 
 
+struct gNodeX { 
+	RB_ENTRY(gNodeX) entry; 
+	RB_HEAD(gNodeYHead, gNodeY) head; 
+	int i,x; 
+}; 
+ 
 
+ 
+ 
 int 
-intcmp(struct testStructure *e1, struct testStructure *e2) 
+intcmpX(struct gNodeX *e1, struct gNodeX *e2) 
 { 
 	if(e1->x < e2->x)
 	{
@@ -48,71 +59,160 @@ intcmp(struct testStructure *e1, struct testStructure *e2)
 		
 		return 0; 
 	}
-	
-}; 
-
-
-
-//this is confusing because it is a macro
-SPLAY_HEAD(inittree, testStructure) head = SPLAY_INITIALIZER(&head);
-
-SPLAY_GENERATE(inittree, testStructure, entry, intcmp);
-
-
-void 
-print_tree(struct testStructure *n) 
-{ 
-	struct testStructure *left, *right; 
+} 
  
-	if (n == NULL) { 
-		printf("nil"); 
-		return; 
-	} 
-	left = SPLAY_LEFT(n, entry); 
-	right = SPLAY_RIGHT(n, entry); 
-	if (left == NULL && right == NULL) 
-		printf("%d", n->x); 
-	else { 
-		printf("%d(", n->x); 
-		print_tree(left); 
-		printf(","); 
-		print_tree(right); 
-		printf(")"); 
-	} 
-}; 
+ 
+int 
+intcmpY(struct gNodeY *e1, struct gNodeY *e2) 
+{ 
+		if(e1->y < e2->y)
+	{
+		return -1;
+	}
+	else if(e1->y > e2->y)
+	{
+		
+		return 1;
+	}
+	else
+	{
+		
+		return 0; 
+	}
+	
+} 
+ 
+RB_HEAD(gNodeXHead, gNodeX) head; 
+RB_GENERATE(gNodeXHead, gNodeX, entry, intcmpX) 
+RB_GENERATE(gNodeYHead, gNodeY, entry, intcmpY) 
+ 
 
 
+
+
+int addGNode();
+
+int generateNodes(int j); 
+
+int returnRandIntInRange(int low, int high)
+{
+		volatile int randomVariable;
+		do
+		{
+			randomVariable = (rand() %  (high - low + 1 ) + low); //return this right here 
+		}
+		
+		while ( !(randomVariable < low && randomVariable > high ));
+		
+		return randomVariable; 
+		
+}
+
+
+struct graphicNodeData *findGNode(int x, int y)
+{
+//this is to actually find the graphical node
+	
+	struct gNodeX tmpXGNode;
+	struct gNodeY tmpYGNode;
+	
+	struct gNodeX *returnXGNode;
+	struct gNodeY *returnYGNode;
+	
+	struct graphicNodeData *returnedGraphicNodeData; //this is the actual data from the y node
+	tmpXGNode.x = x; 
+	tmpYGNode.y = y;
+	 
+	returnXGNode = RB_FIND(gNodeXHead, &head, &tmpXGNode); //this works 
+	if (returnXGNode == NULL) {return NULL;} 
+	
+	
+	//~ RB_INSERT(gNodeYHead,,); 
+	returnYGNode  = RB_FIND(gNodeYHead, &(returnXGNode)->head, &tmpYGNode); //this works 
+
+	if (returnYGNode == NULL) {return NULL;} 
+	
+	return returnYGNode->gnodePointer; 	//return this right here
+};  
+
+//~ int findGNodeY(struct gNodeY *e1);
+ 
+int findGNodeX(); 
+int removeGNode(); 
+int moveGNode(); 
+
+void insertGNodeInTree(int x, int y, struct graphicNodeData *GNode )
+{
+	
+	
+	volatile int internalX, internalY;
+	
+	struct gNodeX tmpXGNode;
+	struct gNodeY tmpYGNode;
+	
+	struct gNodeX *returnXGNode;
+	struct gNodeY *returnYGNode;
+	 
+	struct gNodeX *tmpXGNode = malloc(sizeof(struct gNodeX) *1 );
+	struct gNodeY *tmpYGNode = malloc(sizeof(struct gNodeY) *1 );
+	tmpYGNode->gnodePointer = GNode; 
+	//~ GNode->x; 
+	//~ GNode->y; 
+	
+	//~ tmpXGNode
+	RB_INSERT(gNodeXHead, &head, n);
+	
+	
+
+	
+}
+//~ findGNodeX 
+
+//~ RB_HEAD(gNodeXHead, gNodeX) xHead;
+
+
+struct graphicNodeData mainNodes[100];
 
 int main(int argc, char* argv[])
 {
-	struct testStructure *n1, *n2, *n3, *n4; 
-	//
-	n1 = malloc(sizeof(struct testStructure) * 1); 
-	n2 = malloc(sizeof(struct testStructure) * 1); 
-	n3 = malloc(sizeof(struct testStructure) * 1); 
-	n4 = malloc(sizeof(struct testStructure) * 1); 
 	
-	n1->x = 1;
-	n2->x = 2;
-	n3->x = 3;
-	n4->x = 4;
+	struct gNodeX *omg = malloc(sizeof(struct gNodeX) *1 );
+	struct gNodeY *yee = malloc(sizeof(struct gNodeY) *1 );
 	
-	// // 
-	SPLAY_INSERT(inittree, &head, n1); 
-	SPLAY_INSERT(inittree, &head, n2); 
-	SPLAY_INSERT(inittree, &head, n3); 
-	SPLAY_INSERT(inittree, &head, n4); 
 
-	print_tree(SPLAY_ROOT(&head));
-	
-	
-	
-	
-	free(n1);
-	free(n2);
-	free(n3);
-	free(n4);
-	
+
+//~ void 
+//~ print_tree(struct node *n) 
+//~ { 
+	//~ 
+//~ };
+
+	//~ RB_HEAD(gNodeXHead, gNodeX) nuhead  = RB_INITIALIZER(&nuhead);
+	//~ RB_INSERT(gNodeXHead, &nuhead, omg); 
+	//~ SPLAY_INIT(&(omg)->yHead);
+	//~ SPLAY_INSERT(xLocationHead, &xHead, omg); 
+	//~ SPLAY_INSERT(xLocationHead, &xHead, omg); 
+	//~ SPLAY_INSERT(xLocationHead, &xHead, omg); 
+	//~ SPLAY_INSERT(xLocationHead, &xHead, omg); 
+	//~ 
+	//~ 
+	//~ SPLAY_INSERT(yLocationHead, &(omg)->yHead, yee); 
+	//~ 
+	//~ print_treeX(omg);
+	//~ print_treeY(yee); 
+	//~ struct xLocation findKey; 
+	//~ findKey.x = 3;
+	//~ SPLAY_MAX(xLocationHead, &xHead); 
+	//~ 
+	//~ SPLAY_FIND(yLocationHead, &xHead, &findKey);
+	//~ 
+	//~ 
+	//~ free(omg);
+	//~ free(yee); 
+
+	//~ RB_INSERT(inttree, &head, n);
+
+
 	return 0; 	
 } 
 
